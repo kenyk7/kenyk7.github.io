ad5c74c3a03c19e4ae0095ac1152eab91b95df37
kenyk7 <kenykalin@gmail.com>
2015-07-13 21:17:39 -0500

    Add web

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="es-ES">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Keny Romero Acuña</title>
+
+	<link rel="shortcut icon" href="favicon.png" />
+
+	<!-- Bootstrap CSS -->
+	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
+	<link href="https://bootswatch.com/paper/bootstrap.min.css" rel="stylesheet">
+	<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+
+	<link href="assets/css/material.css" rel="stylesheet">
+
+	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+		<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+<body>
+<header id="header">
+	<div class="header-top hidden-xs">
+		<a href="#header" class="logo"><img src="kenyk.png" alt="Keny Romero" width="40" height="40"></a>
+	</div>
+	<nav class="navbar navbar-inverse navbar-material navbar-dark " role="navigation">
+		<div class="container-fluid"><!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand visible-xs" href="#header">Keny Romero</a>
+			</div>
+				
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse navbar-ex1-collapse">
+				<ul class="nav navbar-nav">
+					<li class="active"><a href="#home"><i class="fa fa-lg fa-fw fa-android"></i> Inicio</a></li>
+					<li><a href="#servicios"><i class="fa fa-lg fa-fw fa-cogs"></i> Servicios</a></li>
+					<li><a href="#portafolio"><i class="fa fa-lg fa-fw fa-book"></i> Portafolio</a></li>
+					<li><a href="#social-media"><i class="fa fa-lg fa-fw fa-share"></i> Social Media</a></li>
+				</ul>
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="tel:971361886"><i class="fa fa-lg fa-fw fa-phone-square"></i> 971361886</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="fa fa-lg fa-fw fa-list"></i> Proyectos <b class="caret"></b></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">Isep SCJ</a></li>
+							<li><a href="#">CreatiPro</a></li>
+							<li><a href="#">KenkPlay</a></li>
+							<li><a href="#">Postgrado Perú</a></li>
+							<li><a href="#">Soubate SAC</a></li>
+							<li><a href="#">Chiclayo Inmobiliaria</a></li>
+							<li><a href="#">Congreso Secretarias</a></li>
+							<li><a href="#">IEP Sicán</a></li>
+							<li><a href="#">Prodem Perú</a></li>
+							<li><a href="#">Perú Training Company</a></li>
+							<li><a href="#">Web Abogado</a></li>
+							<li class="divider"></li>
+							<li><a href="#">Yunga</a></li>
+							<li><a href="#">Babbel</a></li>
+							<li><a href="#">CreatiPro v1</a></li>
+							<li><a href="#">CreatiPro v2</a></li>
+							<li><a href="#">Kapp Theme</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div><!-- /.navbar-collapse -->
+		</div>
+	</nav>
+</header>
+
+<section id="home" class="ws">
+	<div class="container">
+		<div class="text-center">
+			<h1>[ <strong>@Kenyk7</strong> ] <br> Keny Kalín Romero Acuña</h1>
+			<p class="p-lg">Desarrollo páginas web con las últimas tecnologías y tendencias de internet, tecnologías como Html5, Css3 (LESS), Responsive web design; dominio de lenguajes de programación: Php5, MySql, JavaScript, Html5 Apis y Phyton. Uso de Frameworks: BootStrap, JQuery, CodeIgniter.</p>
+			<p>
+				<a href="#portafolio" class="btn btn-lg btn-primary btn-dark"><i class="fa fa-lg fa-fw fa-book"></i> Portafolio</a>
+			</p>
+		</div>
+	</div>
+</section>
+
+<section id="servicios">
+	<header class="main-header">
+	    <div class="container">
+	        <h2 class="page-title pull-let">Servicios</h1>
+	    </div>
+	</header>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-md-4">
+				<div class="service text-center">
+					<div class="service-icon">
+						<i class="fa fa-android fa-5x"></i>
+					</div>
+					<h3>Desarrollo de páginas web</h3>
+					<p>Desarrollo diferentes tipos de páginas web ya sea Personales, Corporativas o Landing page y son desarrolladas totalmente a medida</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="service text-center">
+					<div class="service-icon">
+						<i class="fa fa-windows fa-5x"></i>
+					</div>
+					<h3>Rediseños web</h3>
+					<p>Rediseños páginas web para convertirlas en sitios web responsivos y asi sus usuarios puedan acceder de sus diferentes dispositivos.</p>
+				</div>
+			</div>
+			<div class="col-md-4">
+				<div class="service text-center">
+					<div class="service-icon">
+						<i class="fa fa-apple fa-5x"></i>
+					</div>
+					<h3>Posicionamiento web - SEO</h3>
+					<p >Al desarrollar webs responsivas y a medida, podemos mejor organizar nuestras palabras claves y asi obtener un buen posicionamiento en los buscadores.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="promo" class="section-bg section-bg2">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-5">
+				<img src="assets/img/promo_image.png" alt="Cotización de página web" width="" height="" class="img-responsive">
+			</div>
+			<div class="col-sm-7">
+				<h2 class="title-white">Responsive Web Design</h2>
+				<p class="p-lg">Desde que iniciado a desarrollar sitios web, en su gran mayoría y todos en la actualidad son páginas web responsivas y con un grado de Posicionamiento ya que son diseños totalmente a medida.</p>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section id="portafolio">
+	<header class="main-header">
+	    <div class="container">
+	        <h2 class="page-title pull-let">Portafolio</h1>
+	    </div>
+	</header>
+	<div class="container">
+		<div class="row">
+			<div class="col-lg-10 col-lg-offset-1">
+
+				<article class="article-item">
+					<div class="row">
+						<div class="col-md-8 col-sm-7">
+							<img src="capturas/kenyk7.png" alt="kenyk7" width="983" height="483" class="img-responsive">
+						</div>
+						<div class="col-md-4 col-sm-5">
+							<h3>Keny Romero Acuña</h3>
+							<p>Mi página web en github sobre las cosas que hago y mi portafolio de trabajos realizados entre, otros pequeños diseños de Landing page.</p>
+							<p>
+								<a href="#modalpro" data-toggle="modal" data-img="kenyk7.png" class="btn btn-neutro btn-capture"><span class="btn btn-rounded btn-dark"><i class="fa fa-camera"></i></span> Ver Captura</a>
+								<a href="#" target="proyect" class="btn btn-neutro"> <span class="btn btn-rounded btn-blue"><i class="fa fa-arrow-right"></i></span> Ver Proyecto</a>
+							</p>
+						</div>
+					</div>
+				</article>
+	
+			</div>
+		</div>
+	</div>
+</section>
+
+<div class="modal fade" id="modalpro">
+	<div class="modal-dialog modal-lg">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+				<h4 class="modal-title">Captura</h4>
+			</div>
+			<div class="modal-body">
+				<img src="#" alt="Proyecto web" id="img-modal" class="img-responsive">
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<footer id="social-media">
+	<div class="footer-top text-center">
+		<a href="#" class="btn btn-primary"><i class="fa fa-lg fa-fw fa-android"></i> @kenyk7 </a>
+	</div>
+	<div class="footer-content text-center">
+		<div class="footer-social">
+			<a href="https://www.facebook.com/kenyk7" title="facebook" target="_blank"><i class="fa fa-2x fa-fw fa-facebook-square"></i></a>
+			<a href="https://twitter.com/kenyk7" title="twitter" target="_blank"><i class="fa fa-2x fa-fw fa-twitter-square"></i></a>
+			<a href="https://pe.linkedin.com/pub/kenyk-romero/a9/79a/660" title="linkedin" target="_blank"><i class="fa fa-2x fa-fw fa-linkedin-square"></i></a>
+			<a href="https://github.com/kenyk7" title="github" target="_blank"><i class="fa fa-2x fa-fw fa-github-square"></i></a>
+		</div>
+		<div class="copyright">
+			Todos los derechos reservados <em>@kenyk7</em>
+		</div>
+	</div>
+	<a href="#" class="btn btn-rounded btn-rlg btn-lg btn-default btn-top"><i class="fa fa-arrow-up"></i></a>
+</footer>
+<!-- jQuery -->
+<script src="https://code.jquery.com/jquery.js"></script>
+<!-- Bootstrap JavaScript -->
+<script src="https://netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script>
+
+$(".btn-capture").click(function() {
+	var img = $(this).data('img');
+	$("#img-modal").attr('src', 'capturas/full-'+img);
+});
+
+</script>
+</body>
+</html>
